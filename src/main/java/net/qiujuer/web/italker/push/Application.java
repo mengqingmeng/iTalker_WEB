81586f90144ae7eda0eed0e5fc24a5e48959cb0e
mengqingmeng <762577099@qq.com>
2018-05-11 21:45:59 +0800

    需要用的模型创建

--- src/main/java/net/qiujuer/web/italker/push/Application.java
+++ src/main/java/net/qiujuer/web/italker/push/Application.java
@@ -1,6 +1,8 @@
 package net.qiujuer.web.italker.push;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.qiujuer.web.italker.push.provider.AuthRequestFilter;
+import net.qiujuer.web.italker.push.provider.GsonProvider;
 import net.qiujuer.web.italker.push.service.AccountService;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -12,8 +14,12 @@ public class Application extends ResourceConfig{
         packages("net.qiujuer.web.italker.push");
         //packages(AccountService.class.getPackage().getName());
 
+        // 注册我们的全局请求拦截器
+        register(AuthRequestFilter.class);
+
         // 注册Json解析器
-        register(JacksonJsonProvider.class);
+        //register(JacksonJsonProvider.class);
+        register(GsonProvider.class);
 
         // 注册日志打印输出
         register(Logger.class);
